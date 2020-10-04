29a29709a6776332f9be7ffc8abb1bebf8a5d1c3
Anne <anne.k.plummer@gmail.com>
2020-10-04 18:02:16 -0500

    added pages

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+export const Home = () => (
+        <div>
+          <h2>Jump on fridge chase dog then run away</h2>
+          <p>Cat ipsum dolor sit amet, chew foot get suspicious of own shadow then go play with toilette paper. Climb a tree, wait for a fireman jump to fireman then scratch his face. Roll over and sun my belly. I vomit in the bed in the middle of the night lick left leg for ninety minutes, still dirty what the heck just happened, something feels fishy so eat from dog's food.</p>  
+        </div>
+)
+
+
+ 
